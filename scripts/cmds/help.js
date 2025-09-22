9afea21c1a36c0705ebc8529e768f2523025c17d
thabodl26 <thabodlamini080726@gmail.com>
2025-09-23 01:26:59 +0200

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🍂 | ENDY JARVRIS💻 ]";
+const doNotDelete = "[ 🍂 | Thabo Dlamini💻 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
